546316a6af813a39ed77ecbb8e37829cbdd9aeb2
Albino Tonnnina <albino.tonnina@workshare.com>
2017-11-02 09:37:04 +0000

    Heroku - procfile

---
+++ web.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+app.use(express.compress())
+app.use(express.static(path.join(__dirname, 'build/app')))
+
+app.listen(Number(process.env.PORT || 5000))
